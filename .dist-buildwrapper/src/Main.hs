d599f54e7a424e5a5ac8220e4efee6da8e7ac45a
luisbattisti <luisbattisti@gmail.com>
2012-11-21 13:21:35 -0200

    primeira

---
+++ .dist-buildwrapper/src/Main.hs
@@ -0,0 +1,64 @@
+{--
+        Esqueleto de programa para geração de bubble cloud em Haskell.
+        Mais informações em: http://www.inf.ufsm.br/~andrea/elc117-2012b
+--}
+
+module Main where
+
+import Text.Printf -- Oba, Haskell tem printf! :-)
+
+type Point     = (Float,Float)
+type Color     = (Int,Int,Int)
+type Circle    = (Point,Float)
+
+imageWidth :: Int
+imageWidth = 360
+
+imageHeight :: Int
+imageHeight = 360
+
+-- Funcao principal que faz leitura do dataset e gera arquivo SVG
+main :: IO ()
+main = do 
+        strcontent <- readFile infile
+        let pairs = map (span (/= ' ')) (lines strcontent)
+            freqs = readInts (map snd pairs)
+        writeFile outfile (svgCloudGen imageWidth imageHeight freqs)
+        putStrLn "Ok!"
+        where 
+                infile = "dataset.txt"
+                outfile = "tagcloud.svg" 
+
+-- Transforma lista de strings em lista de inteiros
+readInts :: [String] -> [Int]
+readInts ss = map read ss
+
+
+-- Gera o documento SVG da tag cloud, concatenando cabecalho, conteudo e rodape
+svgCloudGen :: Int -> Int -> [Int] -> String
+svgCloudGen w h dataset = 
+        "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n" ++ 
+        "<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n" ++
+        (svgViewBox w h) ++
+        (concat (svgBubbleGen w h dataset)) ++ "</svg>\n"
+        
+
+-- Esta funcao deve gerar a lista de circulos em formato SVG.
+-- A implementacao atual eh apenas um teste que gera um circulo posicionado no meio da figura.
+-- TODO: Alterar essa funcao para usar os dados do dataset.
+svgBubbleGen:: Int -> Int -> [Int] -> [String]
+svgBubbleGen w h dataset = [svgCircle ((fromIntegral w/2, fromIntegral h/2), 10.0)]
+
+
+-- Gera string representando um circulo em SVG. A cor do circulo esta fixa. 
+-- TODO: Alterar esta funcao para mostrar um circulo de uma cor fornecida como parametro.
+svgCircle :: Circle -> String
+svgCircle ((x,y),r) = printf "<circle cx=\"%f\" cy=\"%f\" r=\"%f\" fill=\"rgb(255,50,10)\" />\n" x y r
+
+
+-- Configura o viewBox da imagem e coloca retangulo branco no fundo
+svgViewBox :: Int -> Int -> String
+svgViewBox w h =
+        printf  "<svg width=\"%d\" height=\"%d\" viewBox=\"0 0 %d %d\"" w h w h ++ 
+                " version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">\n" ++
+        printf "<rect x=\"0\" y=\"0\" width=\"%d\" height=\"%d\" style=\"fill:white;\"/>\n" w h

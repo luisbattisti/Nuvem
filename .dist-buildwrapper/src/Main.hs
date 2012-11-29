53039b88c40d827d587b2c5c46e9d1b1764718eb
luisbattisti <luisbattisti@gmail.com>
2012-11-28 22:51:06 -0200

    commit

--- .dist-buildwrapper/src/Main.hs
+++ .dist-buildwrapper/src/Main.hs
@@ -1,31 +1,38 @@
 {--
-        Esqueleto de programa para geração de bubble cloud em Haskell.
-        Mais informações em: http://www.inf.ufsm.br/~andrea/elc117-2012b
+        Esqueleto de programa para geraÃ§Ã£o de bubble cloud em Haskell.
+        Mais informaÃ§Ãµes em: http://www.inf.ufsm.br/~andrea/elc117-2012b
 --}
+
+--Luis Fernando Battisti de Araujo e Silva
+--Matrícula: 2012510280
+--link da página da disciplina: http://www-usr.inf.ufsm.br/~andrea/elc117/
+
+--Trabalho 3: Tagcloud
+--Entrega: 26/11/2012
 
 module Main where
 
 import Text.Printf -- Oba, Haskell tem printf! :-)
 import System.Random
 import System.IO.Unsafe
-import Data.List
 
 type Point     = (Float,Float)
 type Color     = (Int,Int,Int)
 type Circle    = (Point,Float)
 
-imageWidth :: Int
+imageWidth :: Float
 imageWidth = 360
 
-imageHeight :: Int
+imageHeight :: Float
 imageHeight = 360
+
 
 -- Funcao principal que faz leitura do dataset e gera arquivo SVG
 main :: IO ()
 main = do 
         strcontent <- readFile infile
         let pairs = map (span (/= ' ')) (lines strcontent)
-            freqs = readInts (map snd pairs)
+            freqs = readFloat (map snd pairs)
         writeFile outfile (svgCloudGen imageWidth imageHeight freqs)
         putStrLn "Ok!"
         where 
@@ -33,30 +40,56 @@ main = do
                 outfile = "tagcloud.svg"
 
 
--- Transforma lista de strings em lista de inteiros
-readInts :: [String] -> [Int]
-readInts ss = map read ss
+-- Transforma lista de strings em lista de Float
+readFloat :: [String] -> [Float]
+readFloat ss = map read ss
 
 
 -- Gera o documento SVG da tag cloud, concatenando cabecalho, conteudo e rodape
-svgCloudGen :: Int -> Int -> [Int] -> String
+svgCloudGen :: Float -> Float -> [Float] -> String
 svgCloudGen w h dataset = 
         "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n" ++ 
         "<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n" ++
         (svgViewBox w h) ++
-        (concat (svgBubbleGen w h dataset)) ++ "</svg>\n"
+        (concat (svgBubbleGen [] 0 dataset ) ) ++ "</svg>\n"
 
-        
+
 -- Esta funcao deve gerar a lista de circulos em formato SVG.
 -- A implementacao atual eh apenas um teste que gera um circulo posicionado no meio da figura.
 -- TODO: Alterar essa funcao para usar os dados do dataset.
-svgBubbleGen:: Int -> Int -> [Int] -> [String]
-svgBubbleGen w h dataset = map svgCircle [((fromIntegral w/2, fromIntegral h/2), raios (dataset))]
+svgBubbleGen :: [Circle] -> Float -> [Float] -> [String]
+svgBubbleGen _ _ dataset
+    |dataset == [] = []
+svgBubbleGen listacir t dataset = 
+        let cir = posicaocirculo  t (head dataset/15) listacir
+         in [svgCircle (cir, (head dataset/15)+3)] ++ (svgBubbleGen ([(cir, (head dataset/15)+3)] ++ listacir) t (tail dataset))
 
 
-raios :: [Int] -> [Float]
-raios [] = []
-raios dataset = head (map (fromIntegral) (dataset))/50 : raios (tail dataset)
+--Move em espiral a partir da posição
+posicaocirculo :: Float -> Float -> [Circle] -> Point
+posicaocirculo t r listacir = if (intersecao ((x,y),r) listacir) then posicaocirculo (t + 0.2) r listacir else (x,y)
+                              where x = (1 * t * (cos t) +180)
+                                    y = (1 * t * (sin t) +180)
+
+    
+--Se a soma do raio for maior que a distancia então é verdadeiro...
+intersecao :: Circle -> [Circle] -> Bool
+intersecao circulo listacir = if listacir == [] then False else auxintersecao circulo listacir
+
+auxintersecao :: Circle -> [Circle] -> Bool
+auxintersecao circulo listacir = if ((distancia (fst circulo) (fst (head listacir))) < ((snd circulo) + (snd (head listacir)))) then True else intersecao circulo (tail listacir)
+
+--Distância entre dois pontos (P e Q) -> PeQ = sqrt (x2 - x1)² + (y2 - y1)²
+distancia :: Point -> Point -> Float
+distancia p q = let x = ((fst p) - (fst q)) ^ 2 
+                    y = ((snd p) - (snd q)) ^ 2
+                 in sqrt (x + y) 
+
+
+-- Gera string representando um circulo em SVG. A cor do circulo esta fixa. 
+-- TODO: Alterar esta funcao para mostrar um circulo de uma cor fornecida como parametro. 
+svgCircle :: Circle -> String
+svgCircle ((x,y),r)= printf "<circle cx=\"%f\" cy=\"%f\" r=\"%f\" fill=\"rgb(%d,%d,%d)\" />\n" x y r (unsafePerformIO randomcor) (unsafePerformIO randomcor) (unsafePerformIO randomcor)
 
 
 -- gera um numero aleatorio de 0 até 255        
@@ -64,21 +97,9 @@ randomcor :: IO Int
 randomcor = randomRIO (0, 255 :: Int)
 
 
--- Gera string representando um circulo em SVG. A cor do circulo esta fixa. 
--- TODO: Alterar esta funcao para mostrar um circulo de uma cor fornecida como parametro.
-svgCircle :: Circle -> String
-svgCircle ((x,y),r) = printf "<circle cx=\"%f\" cy=\"%f\" r=\"%f\" fill=\"rgb(%d,%d,%d)\" />\n" x y r (unsafePerformIO randomcor) (unsafePerformIO randomcor) (unsafePerformIO randomcor) 
---unsafePerformIO converte arquivos IO a -> a. 
-
-
 -- Configura o viewBox da imagem e coloca retangulo branco no fundo
-svgViewBox :: Int -> Int -> String
+svgViewBox :: Float -> Float -> String
 svgViewBox w h =
-        printf  "<svg width=\"%d\" height=\"%d\" viewBox=\"0 0 %d %d\"" w h w h ++ 
+        printf  "<svg width=\"%f\" height=\"%f\" viewBox=\"0 0 %f %f\"" w h w h ++ 
                 " version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">\n" ++
-        printf "<rect x=\"0\" y=\"0\" width=\"%d\" height=\"%d\" style=\"fill:white;\"/>\n" w h
-        
- 
---Calcula a distância entre dois pontos - sqrt (x1 - x2)² + (y1 - y2)² 
-distancia :: Circle -> Circle -> Float
-distancia ((x1,y1),_) ((x2,y2),_) = sqrt (((x1 - x2)^2) + ((y1 - y2)^2))
+        printf "<rect x=\"0\" y=\"0\" width=\"%f\" height=\"%f\" style=\"fill:white;\"/>\n" w h
